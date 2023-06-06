c664e2ad8da43e9157b1daaefa474e8d2e3d347a
pedrolima <pedro.lima@picpay.com>
2023-06-05 22:08:01 -0300

    crud de pessoa

--- src/main/java/moderna/cadastro/controller/PessoaController.java
+++ src/main/java/moderna/cadastro/controller/PessoaController.java
@@ -4,8 +4,9 @@ import moderna.cadastro.model.Pessoa;
 import moderna.cadastro.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -14,18 +15,34 @@ public class PessoaController {
     @Autowired //spring gerenciar nossas instancias de repositório.
     private PessoaRepository pessoaRepository;
 
-
     @GetMapping("/texto1")
     public String mostrarTexto(){
         return "sejam bem-vindos a minha primeira API Rest";
     }
 
     @PostMapping("/salvar")
-    public void salvar(@RequestBody Pessoa pessoa){
-        pessoaRepository.save(pessoa);
+    public Pessoa salvar(@RequestBody Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
     }
 
+    @GetMapping("listar-todos")
+    public List<Pessoa> listarTodos(){
+        return pessoaRepository.findAll();
+    }
 
+    @GetMapping("buscar/{id}")
+    public Optional<Pessoa> buscarPorId(@PathVariable Long id){
+        return pessoaRepository.findById(id);
+    }
 
+    @DeleteMapping("deletar-todos")
+    public void deletarTodos(){
+        pessoaRepository.deleteAll();
+    }
+
+    @DeleteMapping("deletar/{id}")
+    public void deletarPorId(@PathVariable Long id){
+        pessoaRepository.deleteById(id);
+    }
 
 }
